bdfe5737f4f23d37ad5d1cae5d680b07546296fc
Brian <bstanton773@gmail.com>
2023-01-20 15:31:14 -0600

    Add display and build new card for country

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ async function handleFormSubmit(e){
     console.log(countryName);
 
     let countryInfo = await getCountryInfo(countryName);
-    console.log(countryInfo);
+    buildCountryCard(countryInfo);
     // Clear the input box at the end
     e.target.countryName.value = '';
 };
@@ -83,3 +83,50 @@ async function getCountryInfo(countryName){
 //         .then(res => res.json())
 //         .then(data => data[0])
 // }
+
+// Function that will take in country object and build an HTML card and add to gallery
+function buildCountryCard(countryObj){
+
+    // Create a card div
+    let card = document.createElement('div')
+    card.className = 'card h-100';
+
+    // Create a top image for card
+    let image = document.createElement('img');
+    image.className = 'card-img-top';
+    image.src = countryObj.flags.png;
+    // Add image as a child to the card
+    card.append(image);
+    
+    // Create card body
+    let cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    
+    // Create country name and population elements
+    let countryTitle = document.createElement('h5');
+    countryTitle.className = 'card-title';
+    countryTitle.innerHTML = countryObj.name.official;
+    
+    let countryPopulation = document.createElement('p');
+    countryPopulation.className = 'card-text';
+    countryPopulation.innerHTML = `Population: ${countryObj.population.toLocaleString('en-US')}`
+    
+    // Add the title and population to the card body
+    cardBody.append(countryTitle);
+    cardBody.append(countryPopulation);
+    
+    // Add card body to card
+    card.append(cardBody);
+
+    // Create a column for the row
+    let col = document.createElement('div')
+    col.className = 'col-12 col-md-6 col-lg-3 my-3'
+
+    // Add the card to the column
+    col.append(card);
+
+    // Get the country display and add the column
+    let display = document.getElementById('countryDisplay');
+    display.append(col);
+
+}
